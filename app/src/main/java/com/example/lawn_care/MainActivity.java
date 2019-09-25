ef0957afd7286145f12aa0237fab3dda10081ac8
AndySana <fh1024@wayne.edu>
2019-09-25 13:45:38 -0400

    UI Work on Buttons and Layouts

--- app/src/main/java/com/example/lawn_care/MainActivity.java
+++ app/src/main/java/com/example/lawn_care/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.lawn_care;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void buttonPress(View view){
+        startActivity(new Intent(MainActivity.this, dash.class));
+        }
 }
