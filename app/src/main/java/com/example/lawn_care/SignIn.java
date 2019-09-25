302807356a528df7f082acacb973308fdd04aaf8
Siddh Patel <gx5234@wayne.edu>
2019-09-25 14:19:17 -0400

    Final Sign In & Sign Up

--- app/src/main/java/com/example/lawn_care/SignIn.java
+++ app/src/main/java/com/example/lawn_care/SignIn.java
@@ -7,17 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in);
         Button btn = (Button) findViewById(R.id.button);
     }
         public void abc(View view) {
-            Intent intent = new Intent(MainActivity.this, SignUp.class);
+            Intent intent = new Intent(SignIn.this, SignUp.class);
             startActivity(intent);
-
         }
 }
