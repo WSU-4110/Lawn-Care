a02c81e4431b792987b54996532641a6acfc6738
Michael Yousif <gb2180@wayne.edu>
2019-10-07 23:15:23 -0400

    Added invalid input handling in the signup page

--- app/src/main/java/com/example/lawn_care/SignUp.java
+++ app/src/main/java/com/example/lawn_care/SignUp.java
@@ -13,6 +13,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -26,6 +28,7 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class SignUp extends AppCompatActivity {
     private EditText ET_password;
@@ -33,6 +36,9 @@ public class SignUp extends AppCompatActivity {
     private EditText ET_firstName;
     private EditText ET_lastName;
     private EditText ET_email;
+    private EditText ET_phoneNumber;
+    private RadioGroup WorkerOwnerRG;
+    private RadioButton WorkerOwnerRB;
     private Button btn_Submit;
 
     @Override
@@ -45,10 +51,14 @@ public class SignUp extends AppCompatActivity {
         ET_firstName = findViewById(R.id.ET_firstName);
         ET_lastName = findViewById(R.id.ET_lastName);
         ET_email = findViewById(R.id.ET_email);
+        ET_phoneNumber=findViewById(R.id.ET_phoneNumber);
         ET_password = findViewById(R.id.ET_password);
         ET_mathPassword = findViewById(R.id.ET_mathPassword);
+        WorkerOwnerRG=findViewById(R.id.WorkerOwnerRG);
 
         ET_mathPassword.addTextChangedListener(passwordWatcher);
+
+
     }
 
     private final TextWatcher passwordWatcher = new TextWatcher() {
@@ -93,24 +103,51 @@ public class SignUp extends AppCompatActivity {
     }
 
     public void signupSubmit(View view) {
-        //IMPORTANT
-        //FORGOT TO INCLUDE PHONE NUMBER
-        //We can include it in the new ui, but don't forget to add it to the parameters.
-        //ALSO NEED TO PASS USER STATE
-        //again, a UI issue, but dont forget
         final String email= ET_email.getText().toString();
         final String password= ET_password.getText().toString();
+        String strMatchPassword = ET_mathPassword.getText().toString();
         final String firstName= ET_firstName.getText().toString();
         final String lastName= ET_lastName.getText().toString();
-        //Log.d("BOOLEAN",String.valueOf(email.length()));
-        if((email.length()==0)||(password.length()==0)||(firstName.length()==0)||(lastName.length()==0)){
+        final String phoneNumber=ET_phoneNumber.getText().toString();
+        WorkerOwnerRB=findViewById(WorkerOwnerRG.getCheckedRadioButtonId());
+        //check that all fields are not empty
+        if((email.length()==0)||(password.length()==0)||(firstName.length()==0)||(lastName.length()==0)||(phoneNumber.length()==0)||WorkerOwnerRB.equals(null)){
             AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
-            builder.setMessage("No field can be blank")
+            builder.setMessage("No field can be empty")
                     .setNegativeButton("Enter info",null)
                     .create()
                     .show();
             return;
         }
+        //check for valid fields
+        String invalidFieldMessage="";
+        if(phoneNumber.length()!=10){
+            invalidFieldMessage="Phone number must be 10 digits long";
+        }
+        String phoneRegex="\\d+";
+        if(!phoneNumber.matches(phoneRegex)){
+            invalidFieldMessage="Phone number must only contain digits";
+        }
+        String emailRegex="^(.+)@(.+)$";
+        if(!email.matches(emailRegex)){
+            invalidFieldMessage="Email is not formatted properly";
+        }
+        if(!password.equals(strMatchPassword)){
+            invalidFieldMessage="Passwords do not match";
+        }
+        if(!invalidFieldMessage.equals("")){
+            AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
+            builder.setMessage(invalidFieldMessage)
+                    .setNegativeButton("Re-enter info",null)
+                    .create()
+                    .show();
+            return;
+        }
+
+
+        final String userTypeInput=WorkerOwnerRB.getText().toString();
+        //Log.d("BOOLEAN",String.valueOf(email.length()));
+
         //we also should check for valid email input, phone, etc.
 
         final String signup_url="http://lawn-care.us-east-1.elasticbeanstalk.com/signup.php";
@@ -165,6 +202,8 @@ public class SignUp extends AppCompatActivity {
                 params.put("password", password);
                 params.put("firstName", firstName);
                 params.put("lastName", lastName);
+                params.put("phoneNumber", phoneNumber);
+                params.put("userType",userTypeInput.equals("Job Seeker")?"worker":"owner");
                 return params;
             }
         };
