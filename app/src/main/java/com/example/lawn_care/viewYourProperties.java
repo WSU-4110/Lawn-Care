ab1b8e94eafa65467ba9ad0f073d84006deeb887
Michael Yousif <gb2180@wayne.edu>
2019-10-09 19:15:27 -0400

    Changes to viewYourProperties

--- app/src/main/java/com/example/lawn_care/viewYourProperties.java
+++ app/src/main/java/com/example/lawn_care/viewYourProperties.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -42,10 +43,9 @@ public class viewYourProperties extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_your_properties);
-    }
 
-    public void viewYourData(View view)
-    {
+
+        //your code needs to be here because it should fill the screen on creation, aka when they open this view
         final String email = localUserInfo.getEmail();
 
         final String getInfo_url="http://lawn-care.us-east-1.elasticbeanstalk.com/viewYourListings.php";
@@ -58,7 +58,29 @@ public class viewYourProperties extends AppCompatActivity {
                             jsonResponse = new JSONObject(response);
                             if (jsonResponse.getString("success") != "false") {
                                 //success toast
-                                Toast.makeText(getApplicationContext(), "Successfully Connected", Toast.LENGTH_LONG).show();
+                                Toast.makeText(getApplicationContext(), "Successfully Retrieved Data", Toast.LENGTH_LONG).show();
+
+                                //DO SOMETHING HERE!!!!!
+                                //your page will be blank if there is nothing here!
+                                /*
+                                //Just an idea...
+                                //see this for some details https://stackoverflow.com/questions/4576848/creating-linear-layout-with-textviews-using-a-for-loop
+                                //gets the linear layout from the already existing one in the view
+                                LinearLayout linearLayout=findViewById(R.id.LL_yourProperties);
+                                for each item from 0 to n in the json{
+                                    LinearLayout listingItem= new LinearLayout(this);
+                                    listingItem.setOrientation(vertical);
+                                    TextView address=new TextView(this);
+                                    TextView workNeeded=new TextView(this);
+                                    address.setText(stuff from json);
+                                    workNeeded.setText(stuff from json);
+                                    listingItem.addView(address);
+                                    listingItem.addView(workNeeded);
+                                    linearLayout.addView(listingItem);
+                                }
+
+
+                                 */
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -79,13 +101,20 @@ public class viewYourProperties extends AppCompatActivity {
                 }){
             @Override
             protected Map<String,String> getParams(){
-              Map<String,String> params= new HashMap<String,String>();
-              params.put("email", email);
-              params.put("null", null);
-              return params;
+                Map<String,String> params= new HashMap<String,String>();
+                params.put("email", email);
+                return params;
             }
         };
         RequestQueue requestQueue=Volley.newRequestQueue(viewYourProperties.this);
         requestQueue.add(stringRequest);
     }
+
+    //this function is never called from anywhere, the button on the dash calls a function with this name on the dash, not on this page
+    /*
+    public void viewYourData(View view)
+    {
+
+    }
+     */
 }
