6edbfc14e5bb354d61223b09b1f617a54383a221
AndySana <fh1024@wayne.edu>
2019-11-03 13:11:00 -0500

    Finish up work on Edit Your Property Backend

--- app/src/main/java/com/example/lawn_care/editYourProperty.java
+++ app/src/main/java/com/example/lawn_care/editYourProperty.java
@@ -31,6 +31,12 @@ import java.util.Map;
 public class editYourProperty extends AppCompatActivity {
 
     int propertyNumber;
+    String addressEdit;
+    String stateEdit;
+    String zipEdit;
+    String lawnSizeEdit;
+    String cityEdit;
+    String workNeededEdit;
 
     private Spinner spinnerState;
     private EditText ET_address;
@@ -64,7 +70,16 @@ public class editYourProperty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_property);
+
+        //get Intent Extras
         propertyNumber = getIntent().getIntExtra("EXTRA_NUM",-1);
+        addressEdit = getIntent().getStringExtra("EXTRA_STREET");
+        zipEdit = getIntent().getStringExtra("EXTRA_ZIP");
+        lawnSizeEdit = getIntent().getStringExtra("EXTRA_LAWNSIZE");
+        cityEdit = getIntent().getStringExtra("EXTRA_CITY");
+        stateEdit = getIntent().getStringExtra("EXTRA_STATE");
+        workNeededEdit = getIntent().getStringExtra("EXTRA_WORK");
+
         ET_address = findViewById(R.id.ET_address);
         ET_city = findViewById(R.id.ET_city);
         ET_zipcode = findViewById(R.id.ET_zipCode);
@@ -89,17 +104,410 @@ public class editYourProperty extends AppCompatActivity {
 
         spinnerState = findViewById(R.id.spinner2);
 
-    }
 
-    public void ResetAll(View view) {
-        spinnerState.setSelection(0,true);
-        ET_address.setText("");
-        ET_city.setText("");
-        ET_zipcode.setText("");
-        ET_PropertySize.setText("");
+
+        switch(stateEdit)
+        {
+            case "AL":
+                spinnerState.setSelection(1,true);
+                break;
+            case "AK":
+                spinnerState.setSelection(2,true);
+                break;
+            case "AZ":
+                spinnerState.setSelection(3,true);
+                break;
+            case "AR":
+                spinnerState.setSelection(4,true);
+                break;
+            case "CA":
+                spinnerState.setSelection(5,true);
+                break;
+            case "CO":
+                spinnerState.setSelection(6,true);
+                break;
+            case "CT":
+                spinnerState.setSelection(7,true);
+                break;
+            case "DE":
+                spinnerState.setSelection(8,true);
+                break;
+            case "DC":
+                spinnerState.setSelection(9,true);
+                break;
+            case "FL":
+                spinnerState.setSelection(10,true);
+                break;
+            case "GA":
+                spinnerState.setSelection(11,true);
+                break;
+            case "HI":
+                spinnerState.setSelection(12,true);
+                break;
+            case "ID":
+                spinnerState.setSelection(13,true);
+                break;
+            case "IL":
+                spinnerState.setSelection(14,true);
+                break;
+            case "IN":
+                spinnerState.setSelection(15,true);
+                break;
+            case "IA":
+                spinnerState.setSelection(16,true);
+                break;
+            case "KS":
+                spinnerState.setSelection(17,true);
+                break;
+            case "KY":
+                spinnerState.setSelection(18,true);
+                break;
+            case "LA":
+                spinnerState.setSelection(19,true);
+                break;
+            case "ME":
+                spinnerState.setSelection(20,true);
+                break;
+            case "MD":
+                spinnerState.setSelection(21,true);
+                break;
+            case "MA":
+                spinnerState.setSelection(22,true);
+                break;
+            case "MI":
+                spinnerState.setSelection(23,true);
+                break;
+            case "MN":
+                spinnerState.setSelection(24,true);
+                break;
+            case "MS":
+                spinnerState.setSelection(25,true);
+                break;
+            case "MO":
+                spinnerState.setSelection(26,true);
+                break;
+            case "MT":
+                spinnerState.setSelection(27,true);
+                break;
+            case "NE":
+                spinnerState.setSelection(28,true);
+                break;
+            case "NV":
+                spinnerState.setSelection(29,true);
+                break;
+            case "NH":
+                spinnerState.setSelection(30,true);
+                break;
+            case "NJ":
+                spinnerState.setSelection(31,true);
+                break;
+            case "NM":
+                spinnerState.setSelection(32,true);
+                break;
+            case "NY":
+                spinnerState.setSelection(33,true);
+                break;
+            case "NC":
+                spinnerState.setSelection(34,true);
+                break;
+            case "ND":
+                spinnerState.setSelection(35,true);
+                break;
+            case "OH":
+                spinnerState.setSelection(36,true);
+                break;
+            case "OK":
+                spinnerState.setSelection(37,true);
+                break;
+            case "OR":
+                spinnerState.setSelection(38,true);
+                break;
+            case "PA":
+                spinnerState.setSelection(39,true);
+                break;
+            case "RI":
+                spinnerState.setSelection(40,true);
+                break;
+            case "SC":
+                spinnerState.setSelection(41,true);
+                break;
+            case "SD":
+                spinnerState.setSelection(42,true);
+                break;
+            case "TN":
+                spinnerState.setSelection(43,true);
+                break;
+            case "TX":
+                spinnerState.setSelection(44,true);
+                break;
+            case "UT":
+                spinnerState.setSelection(45,true);
+                break;
+            case "VT":
+                spinnerState.setSelection(46,true);
+                break;
+            case "VA":
+                spinnerState.setSelection(47,true);
+                break;
+            case "WA":
+                spinnerState.setSelection(48,true);
+                break;
+            case "WV":
+                spinnerState.setSelection(49,true);
+                break;
+            case "WI":
+                spinnerState.setSelection(50,true);
+                break;
+            case "WY":
+                spinnerState.setSelection(51,true);
+                break;
+            default:
+                spinnerState.setSelection(0,true);
+        }
+
+        ET_address.setText(addressEdit);
+        ET_city.setText(cityEdit);
+        ET_zipcode.setText(zipEdit);
+        ET_PropertySize.setText(lawnSizeEdit);
         ET_JobType.setText("");
         ET_Tools.setText("");
 
+
+        if (workNeededEdit.contains("Clippings") == true)
+            CB_clippings.setChecked(true);
+        else
+            CB_clippings.setChecked(false);
+        if (workNeededEdit.contains("Trimming") == true)
+            CB_trimming.setChecked(true);
+        else
+            CB_trimming.setChecked(false);
+        if (workNeededEdit.contains("Mowing") == true)
+            CB_mowing.setChecked(true);
+        else
+            CB_mowing.setChecked(false);
+        if (workNeededEdit.contains("Fertilization") == true)
+            CB_fertilization.setChecked(true);
+        else
+            CB_fertilization.setChecked(false);
+        if (workNeededEdit.contains("Weed_Control") == true)
+            CB_weed_Control.setChecked(true);
+        else
+            CB_weed_Control.setChecked(false);
+        if (workNeededEdit.contains("Pest_Control") == true)
+            CB_pest_Control.setChecked(true);
+        else
+            CB_pest_Control.setChecked(false);
+        if (workNeededEdit.contains("Irrigation") == true)
+            CB_irrigation.setChecked(true);
+        else
+            CB_irrigation.setChecked(false);
+        if (workNeededEdit.contains("Aeration") == true)
+            CB_aeration.setChecked(true);
+        else
+            CB_aeration.setChecked(false);
+
+    }
+
+    public void ResetAll(View view) {
+        switch(stateEdit)
+        {
+            case "AL":
+                spinnerState.setSelection(1,true);
+                break;
+            case "AK":
+                spinnerState.setSelection(2,true);
+                break;
+            case "AZ":
+                spinnerState.setSelection(3,true);
+                break;
+            case "AR":
+                spinnerState.setSelection(4,true);
+                break;
+            case "CA":
+                spinnerState.setSelection(5,true);
+                break;
+            case "CO":
+                spinnerState.setSelection(6,true);
+                break;
+            case "CT":
+                spinnerState.setSelection(7,true);
+                break;
+            case "DE":
+                spinnerState.setSelection(8,true);
+                break;
+            case "DC":
+                spinnerState.setSelection(9,true);
+                break;
+            case "FL":
+                spinnerState.setSelection(10,true);
+                break;
+            case "GA":
+                spinnerState.setSelection(11,true);
+                break;
+            case "HI":
+                spinnerState.setSelection(12,true);
+                break;
+            case "ID":
+                spinnerState.setSelection(13,true);
+                break;
+            case "IL":
+                spinnerState.setSelection(14,true);
+                break;
+            case "IN":
+                spinnerState.setSelection(15,true);
+                break;
+            case "IA":
+                spinnerState.setSelection(16,true);
+                break;
+            case "KS":
+                spinnerState.setSelection(17,true);
+                break;
+            case "KY":
+                spinnerState.setSelection(18,true);
+                break;
+            case "LA":
+                spinnerState.setSelection(19,true);
+                break;
+            case "ME":
+                spinnerState.setSelection(20,true);
+                break;
+            case "MD":
+                spinnerState.setSelection(21,true);
+                break;
+            case "MA":
+                spinnerState.setSelection(22,true);
+                break;
+            case "MI":
+                spinnerState.setSelection(23,true);
+                break;
+            case "MN":
+                spinnerState.setSelection(24,true);
+                break;
+            case "MS":
+                spinnerState.setSelection(25,true);
+                break;
+            case "MO":
+                spinnerState.setSelection(26,true);
+                break;
+            case "MT":
+                spinnerState.setSelection(27,true);
+                break;
+            case "NE":
+                spinnerState.setSelection(28,true);
+                break;
+            case "NV":
+                spinnerState.setSelection(29,true);
+                break;
+            case "NH":
+                spinnerState.setSelection(30,true);
+                break;
+            case "NJ":
+                spinnerState.setSelection(31,true);
+                break;
+            case "NM":
+                spinnerState.setSelection(32,true);
+                break;
+            case "NY":
+                spinnerState.setSelection(33,true);
+                break;
+            case "NC":
+                spinnerState.setSelection(34,true);
+                break;
+            case "ND":
+                spinnerState.setSelection(35,true);
+                break;
+            case "OH":
+                spinnerState.setSelection(36,true);
+                break;
+            case "OK":
+                spinnerState.setSelection(37,true);
+                break;
+            case "OR":
+                spinnerState.setSelection(38,true);
+                break;
+            case "PA":
+                spinnerState.setSelection(39,true);
+                break;
+            case "RI":
+                spinnerState.setSelection(40,true);
+                break;
+            case "SC":
+                spinnerState.setSelection(41,true);
+                break;
+            case "SD":
+                spinnerState.setSelection(42,true);
+                break;
+            case "TN":
+                spinnerState.setSelection(43,true);
+                break;
+            case "TX":
+                spinnerState.setSelection(44,true);
+                break;
+            case "UT":
+                spinnerState.setSelection(45,true);
+                break;
+            case "VT":
+                spinnerState.setSelection(46,true);
+                break;
+            case "VA":
+                spinnerState.setSelection(47,true);
+                break;
+            case "WA":
+                spinnerState.setSelection(48,true);
+                break;
+            case "WV":
+                spinnerState.setSelection(49,true);
+                break;
+            case "WI":
+                spinnerState.setSelection(50,true);
+                break;
+            case "WY":
+                spinnerState.setSelection(51,true);
+                break;
+            default:
+                spinnerState.setSelection(0,true);
+        }
+
+
+        ET_address.setText(addressEdit);
+        ET_city.setText(cityEdit);
+        ET_zipcode.setText(zipEdit);
+        ET_PropertySize.setText(lawnSizeEdit);
+        ET_JobType.setText("");
+        ET_Tools.setText("");
+
+        if (workNeededEdit.contains("Clippings") == true)
+            CB_clippings.setChecked(true);
+        else
+            CB_clippings.setChecked(false);
+        if (workNeededEdit.contains("Trimming") == true)
+            CB_trimming.setChecked(true);
+        else
+            CB_trimming.setChecked(false);
+        if (workNeededEdit.contains("Mowing") == true)
+            CB_mowing.setChecked(true);
+        else
+            CB_mowing.setChecked(false);
+        if (workNeededEdit.contains("Fertilization") == true)
+            CB_fertilization.setChecked(true);
+        else
+            CB_fertilization.setChecked(false);
+        if (workNeededEdit.contains("Weed_Control") == true)
+            CB_weed_Control.setChecked(true);
+        else
+            CB_weed_Control.setChecked(false);
+        if (workNeededEdit.contains("Pest_Control") == true)
+            CB_pest_Control.setChecked(true);
+        else
+            CB_pest_Control.setChecked(false);
+        if (workNeededEdit.contains("Irrigation") == true)
+            CB_irrigation.setChecked(true);
+        else
+            CB_irrigation.setChecked(false);
+        if (workNeededEdit.contains("Aeration") == true)
+            CB_aeration.setChecked(true);
+        else
+            CB_aeration.setChecked(false);
 
     }
 
@@ -123,7 +531,6 @@ public class editYourProperty extends AppCompatActivity {
     }
     public void edit(View view)
     {
-
         CB_clippings.setChecked(false);
         CB_trimming.setChecked(false);
         CB_mowing.setChecked(false);
@@ -132,7 +539,6 @@ public class editYourProperty extends AppCompatActivity {
         CB_pest_Control.setChecked(false);
         CB_irrigation.setChecked(false);
         CB_aeration.setChecked(false);
-
     }
     public void PostJob(View view) {
         StringBuffer stringBufferTypesOfWorkOffered = new StringBuffer();
@@ -153,7 +559,8 @@ public class editYourProperty extends AppCompatActivity {
         final String zipcode= ET_zipcode.getText().toString();
         final String propertySize= ET_PropertySize.getText().toString();
         final String jobtype= ET_JobType.getText().toString();
-        final String Tools= ET_Tools.getText().toString();
+        //final String Tools= ET_Tools.getText().toString();
+        final String Tools;
         final String state= spinnerState.getSelectedItem().toString();
         if (CB_clippings.isChecked())
             stringBufferTypesOfWorkOffered.append("Clippings,");
@@ -182,10 +589,14 @@ public class editYourProperty extends AppCompatActivity {
         final String tempTypesOfWorkOffered = stringBufferTypesOfWorkOffered.toString();
 
         if (CB_Yes.isChecked())
-            stringBufferTypesOfWorkOffered.append("Yes");
-        if (CB_No.isChecked())
-            stringBufferTypesOfWorkOffered.append("No");
-
+            //stringBufferTypesOfWorkOffered.append("Yes");
+            Tools="1";
+        else
+            Tools="0";
+             /*
+            if (CB_No.isChecked())
+                stringBufferTypesOfWorkOffered.append("No");
+              */
         final String tempTools = stringBufferToolsAvail.toString();
         final String addListing_url="http://lawn-care.us-east-1.elasticbeanstalk.com/editListing.php";
         //final String addListing_url="http://localhost/Scripts/editListing.php";
@@ -244,7 +655,7 @@ public class editYourProperty extends AppCompatActivity {
                 params.put("zip", zipcode);
                 params.put("lawnSize", propertySize);
                 params.put("equipmentAvailable", Tools);
-                //params.put("workNeeded", jobtype);
+                params.put("workNeeded", tempTypesOfWorkOffered);
                 //params.put("workOffered",tempTypesOfWorkOffered);
                 //params.put("Tools",tempTools);
                 return params;
