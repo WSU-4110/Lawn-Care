34248bd7e34fc1c7cca2c72ad212b64541356a65
IshanPatel444 <55370466+IshanPatel444@users.noreply.github.com>
2019-11-14 16:46:29 -0500

    testing adding map
    test

--- app/src/main/java/com/example/lawn_care/SignIn.java
+++ app/src/main/java/com/example/lawn_care/SignIn.java
@@ -41,22 +41,6 @@ public class SignIn extends AppCompatActivity {
     Button BTN_login;
     //CheckBox CB_showPassword;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
-
-        ET_loginEmail=findViewById(R.id.ET_loginEmail);
-        ET_loginPassword=findViewById(R.id.ET_loginPassword);
-         BTN_login=findViewById(R.id.BTN_login);
-         //CB_showPassword=findViewById(R.id.CB_showPassword);
-    }
-
-    public void ActivitySignUp(View view) {
-        Intent intent = new Intent(SignIn.this, SignUp.class);
-        startActivity(intent);
-    }
-
     public void LoginAttempt(View view) {
         final String email= ET_loginEmail.getText().toString();
         final String password= ET_loginPassword.getText().toString();
@@ -141,6 +125,22 @@ public class SignIn extends AppCompatActivity {
         };
         RequestQueue requestQueue=Volley.newRequestQueue(SignIn.this);
         requestQueue.add(stringRequest);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        ET_loginEmail=findViewById(R.id.ET_loginEmail);
+        ET_loginPassword=findViewById(R.id.ET_loginPassword);
+         BTN_login=findViewById(R.id.BTN_login);
+         //CB_showPassword=findViewById(R.id.CB_showPassword);
+    }
+
+    public void ActivitySignUp(View view) {
+        Intent intent = new Intent(SignIn.this, SignUp.class);
+        startActivity(intent);
     }
 
     /*
--- app/src/main/java/com/example/lawn_care/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/lawn_care/ui/notifications/NotificationsFragment.java
@@ -1,24 +1,49 @@
 package com.example.lawn_care.ui.notifications;
 
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.lawn_care.R;
+import com.example.lawn_care.SignIn;
+import com.example.lawn_care.dash;
+import com.example.lawn_care.localUserInfo;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class NotificationsFragment extends Fragment implements OnMapReadyCallback {
 
@@ -47,11 +72,77 @@ public class NotificationsFragment extends Fragment implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
+        final String signin_url="http://lawn-care.us-east-1.elasticbeanstalk.com/login.php";
+        //stringRequest is an object that contains the request method, the url, and the parameters and the response
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, signin_url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject jsonResponse;
+                        try {
+                            jsonResponse=new JSONObject(response);
+                            if(jsonResponse.getString("success")!="false"){
+                                //save the data from the json into local variables
+                                String firstName=jsonResponse.getString("firstName");
 
 
-        LatLng sydney=new LatLng(42.3314,-83.0458);
-        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                                //here
+                                LatLng sydney=new LatLng(42.3314,-83.0458);
+                                List<Address> list= new ArrayList<>();
+                                Geocoder geocoder=new Geocoder(getActivity());
+                                try {
+                                    list = geocoder.getFromLocationName("2533 Solace Drive, Commerce Township 48382", 1);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                                Address address=list.get(0);
+
+                                googleMap.addMarker(new MarkerOptions().position(new LatLng(address.getLatitude(),address.getLongitude())).title("my house"));
+
+
+                                //googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                                googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                            }
+                            else{
+                                //message for incorrect password
+                                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                                builder.setMessage("Map Issue")
+                                        .setNegativeButton("Try Again",null)
+                                        .create()
+                                        .show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                        builder.setMessage("Connection Failed")
+                                .setNegativeButton("Try Again",null)
+                                .create()
+                                .show();
+                        error.printStackTrace();
+                        Log.e("VOLLEY", error.getMessage());
+                        //requestQueue.stop();
+                    }
+                }){
+            @Override
+            //this function is written to get the parameters for posting
+            protected Map<String,String> getParams(){
+                Map<String,String> params= new HashMap<String, String>();
+                return params;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
+
+
+
     }
 
 
